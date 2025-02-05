5d5ab7c0b7984a29f61333549f2b1d4047aff8e4
Remy Fayet <remy.fayet@telecom-paris.fr>
2025-02-05 14:23:39 +0100

    [day2] add part 1

---
+++ src/solutions/day2.rs
@@ -0,0 +1,58 @@
+#[cfg(test)]
+mod tests {
+    use itertools::Itertools;
+
+    enum Growth {
+        Up,
+        Down,
+        Still,
+    }
+
+    #[test]
+    fn day2_part1() {
+        let source = include_str!("../resources/day2.txt");
+        let reports: Vec<Vec<u32>> = source
+            .lines()
+            .map(|l| {
+                l.split(" ")
+                    .filter_map(|n| n.parse::<u32>().ok())
+                    .collect_vec()
+            })
+            .collect_vec();
+        let res = reports
+            .iter()
+            .filter(|report| match report.len() {
+                0 => false,
+                1 => true,
+                _ => {
+                    let mut growth = if report[0] < report[1] {
+                        Growth::Up
+                    } else if report[0] > report[1] {
+                        Growth::Down
+                    } else {
+                        return false;
+                    };
+                    report
+                        .iter()
+                        .zip(report.iter().skip(1))
+                        .all(|p| match growth {
+                            Growth::Up => (p.0 < p.1) && (p.1 - p.0) <= 3,
+                            Growth::Down => (p.0 > p.1) && (p.0 - p.1) <= 3,
+                            Growth::Still => {
+                                growth = if p.0 < p.1 {
+                                    Growth::Up
+                                } else if p.0 > p.1 {
+                                    Growth::Down
+                                } else {
+                                    Growth::Still
+                                };
+                                true
+                            }
+                        })
+                }
+            })
+            .collect_vec()
+            .len();
+        dbg!(res);
+    }
+}
